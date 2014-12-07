a8fa772c54195ee8dce345c8a5035314dca218f3
pwyszynski <stuntmankg@gmail.com>
2014-12-07 18:48:18 +0100

    NOT working -.-

--- script.rb
+++ script.rb
@@ -1,13 +1,13 @@
 #!/bin/env ruby
 # encoding: utf-8
-#wmoimsercutylkoRuby
+#wmoimsercutylkoRuby (͡°͜ʖ͡°)
 
 class Task <
   Struct.new(:index, :time, :progress, :arrival, :deadline, :aet, :finished)
   #aet - actual end time
 
   def print_task
-		puts "#{index}\t | #{time}\t | #{arrival}   \t | #{deadline}\t | #{aet}\t"
+		puts "I=#{index}\t | T=#{time}\t | P=#{progress}\t | A=#{arrival}\t | D=#{deadline}\t | AET=#{aet}\t | F=#{finished}"
 	end
 
 	def print_task_and_progress
@@ -39,8 +39,11 @@ def Liu(tasklist)
 	len = tasklist.length
 	arrived = Array.new
 
+	# i = jednostka czasu, w ktorej jestesmy
 	for i in (0...totaltime) do
+		# przebiegam cala petle w celu sprawdzenia które zadania właśnie doszły do systemu
 		for j in (0...len) do
+			# jeżeli arrival = currentTime to spushuj to zadanie do tablicy arrived
 			if tasklist[j].arrival = i
 				arrived.push(tasklist[j])
 			end
@@ -49,16 +52,16 @@ def Liu(tasklist)
 		earliest_deadline_index = 0
 
 		#wyszukiwanie najwcześniejszego deadline'u wśród tych, które dotarły
-		for k in (0..arrived.length) do
+		for k in (0...arrived.length) do
 			if (arrived[k].deadline<arrived[earliest_deadline_index].deadline && arrived[k].finished==false)
-				earliest_deadline_index = arrived[k].index
+				earliest_deadline_index = k
 			end
 		end
 
 		arrived[earliest_deadline_index].progress+=1
 		arrived[earliest_deadline_index].aet=i
 
-		arrived[earliest_deadline_index].print_task_and_progress
+		arrived[earliest_deadline_index].print_task
 
 		markFinished(arrived)
 	end
@@ -87,11 +90,11 @@ tasks = Array.new
 # 	tasks.push(z)
 # end
 
-tasks.push(Task.new(1, 2, 0, 0, 2, 0, false)
-tasks.push(Task.new(2, 2, 0, 0, 3, 0, false)
-tasks.push(Task.new(3, 3, 0, 1, 1, 0, false)
-tasks.push(Task.new(4, 1, 0, 2, 4, 0, false)
-tasks.push(Task.new(5, 2, 0, 3, 5, 0, false)
-tasks.push(Task.new(6, 2, 0, 3, 5, 0, false)
+tasks.push(Task.new(1, 2, 0, 0, 2, 0, false))
+tasks.push(Task.new(2, 2, 0, 0, 3, 0, false))
+tasks.push(Task.new(3, 3, 0, 1, 1, 0, false))
+tasks.push(Task.new(4, 1, 0, 2, 4, 0, false))
+tasks.push(Task.new(5, 2, 0, 3, 5, 0, false))
+tasks.push(Task.new(6, 2, 0, 3, 5, 0, false))
 
 Liu(tasks)
